b2db171ccb28839becd4b6e3732c01f96732a152
Kent1an <qkent@anamosa.k12.ia.us>
2026-04-02 16:38:17 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// Create a person object with age 0
+const person = {
+  age: 0
+};
+
+// Function that celebrates a birthday by incrementing the person's age
+function celebrateBirthday(p) {
+  p.age += 1;
+  return p;
+}
+
+// Expose globally (in browser environment it's already global by const/function in non-module script)
 
